a42de9336eff16f7af56df2857feac729c7147e4
Firiko <firikoabebe@gmail.com>
2025-09-18 16:08:47 +0300

    maPush

--- src/App.js
+++ src/App.js
@@ -1,24 +1,9 @@
-//import List from './List.js'
-import TodoList from './TodoList.js'
-
+import UseEffect from './UseEffect.js'
 function App() {
-  const fruits=[{id:1,name:"coconut",calories:55},
-    {id:2,name:"apple",calories:222},
-    {id:3,name:"papaya",calories:888}
-   ];
-    const vegetables=[{id:1,name:"brocolli",calories:55},
-    {id:2,name:"potato",calories:222},
-    {id:3,name:"corn",calories:888}
-   ];
+  
 
-
-  return (<>
-   {/*choose this {fruits.length > 0 ? <List items={fruits} catagory="Fruits"/>: null}
-   {vegetables.length > 0 ? <List items={vegetables} catagory="Vegetables"/>  : null} */}
-  {/*or this  {fruits.length > 0 && <List items={fruits} catagory="Fruits"/>}
-   {vegetables.length > 0 && <List items={vegetables} catagory="Vagetables"/>} */}
-   <TodoList/>
-  </>
+  return (
+   <UseEffect />
   );
 }
 
---
+++ src/UseEffect.js
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from "react";
+
+function UseEffect() {
+  const [count, setCount] = useState(0);
+
+ useEffect(() => {
+    document.title= `🔥 ${count}`
+  } , [count]);
+  function addCount() {
+    setCount(c => c + 1);
+  };
+ 
+
+  return (
+    <>
+      <p>Count: {count}</p>
+      <button onClick={addCount}>Add</button>
+    </>
+  );
+}
+
+export default UseEffect;
